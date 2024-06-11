95da1f24db8606b0164614cf4c2614cb2fef3698
Fernando <gomez.romano@gmail.com>
2024-06-11 11:30:08 +0200

    cambios para pruebas en django.settings

--- ARPBIGIDISBA_frontend/ARPBIGIDISBA_frontend/settings.py
+++ ARPBIGIDISBA_frontend/ARPBIGIDISBA_frontend/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
         'PASSWORD': 'password',  # 'Orgullovalor',
         # 'USER': 'root',
         # 'PASSWORD': 'Orgullovalor',
-        'HOST': 'arpbig_psdb', # '127.0.0.1',
+        'HOST': 'mysql_psdb', # '127.0.0.1',
         'PORT': '3306',
     }
 }
