cb5ddfbe6aa4bb6a371c32490951dfed31fdfda3
fernando <gomez.romano@gmail.com>
2026-03-24 12:14:05 +0100

    cambios en modelo filepath, añadido dump de proyecto COLIMERO en /mysql_data

--- ARPBIGIDISBA_frontend/home/models.py
+++ ARPBIGIDISBA_frontend/home/models.py
@@ -168,7 +168,9 @@ class FilePath(models.Model):
     file_path_id = models.AutoField(primary_key=True)
     isolate_id = models.OneToOneField('MetadataGeneral', models.DO_NOTHING, blank=True, null=True, db_column='isolate_id')
     fastq_path = models.CharField(max_length=255, blank=True, null=True, verbose_name="FASTQ path", db_comment="Path to the folder where FASTQ files of the isolate are stored")
-    denovo_assembly_path = models.CharField(max_length=255, blank=True, null=True, verbose_name="DeNovo files path", db_comment="Path to the folder where de novo files of the isolate are stored")
+    fastq_files_names = models.JSONField(blank=True, null=True, verbose_name="FASTQ file/s name/s", db_comment="Name of the FASTQ files of the isolate, separated by comma if more than one")
+    denovo_assembly_path = models.CharField(max_length=255, blank=True, null=True, verbose_name="De novo files path", db_comment="Path to the folder where de novo files of the isolate are stored")
+    denovo_assembly_ena_url = models.CharField(max_length=255, blank=True, null=True, verbose_name="ENA de novo file url", db_comment="Path to the endpoint in ENA database where assembly file of the isolate is stored")
     assembler = models.ForeignKey(Assembler, models.DO_NOTHING, db_column='assembler_id', blank=True, null=True, db_comment="Ensamblador utilizado")
 
     class Meta:
