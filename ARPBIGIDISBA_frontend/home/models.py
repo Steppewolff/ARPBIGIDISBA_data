8c11554830a5b392bbf1ac302d8910a218159816
fernando <gomez.romano@gmail.com>
2026-03-26 12:30:50 +0100

    cambios en modelo filepath, añadida variable para accession number en ENA

--- ARPBIGIDISBA_frontend/home/models.py
+++ ARPBIGIDISBA_frontend/home/models.py
@@ -172,6 +172,7 @@ class FilePath(models.Model):
     denovo_assembly_path = models.CharField(max_length=255, blank=True, null=True, verbose_name="De novo files path", db_comment="Path to the folder where de novo files of the isolate are stored")
     denovo_assembly_ena_url = models.CharField(max_length=255, blank=True, null=True, verbose_name="ENA de novo file url", db_comment="Path to the endpoint in ENA database where assembly file of the isolate is stored")
     denovo_fastq_ena_url = models.JSONField(blank=True, null=True, verbose_name="ENA FASTQ url", db_comment="JSON with paths to endpoints in ENA database where FASTQ files of the isolate are stored")
+    ena_accession = models.CharField(max_length=100, blank=True, null=True, verbose_name="ENA accession number", db_comment="Accession number of the isolate in ENA database")
     assembler = models.ForeignKey(Assembler, models.DO_NOTHING, db_column='assembler_id', blank=True, null=True, db_comment="Ensamblador utilizado")
 
     class Meta:
